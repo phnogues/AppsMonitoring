09266cdcc2cbdf95d85f0f7a5d8397a0b01a6ee0
Pierre-Henri Nogues <pierrehenri56@gmail.com>
2022-08-29 18:24:56 +0200

    First commit

---
+++ AppsMonitoring.UI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AppsMonitoring.UI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppsMonitoring.UI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AppsMonitoring.UI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
